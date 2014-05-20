55abd0c2a7d12161baeda151bb3a391490ed2dca
nsrirama <nsrirama@ancestry.com>
2014-05-20 11:54:20 -0700

    Jade command added

--- app.js
+++ app.js
@@ -5,6 +5,17 @@ app.get('/', function(req, res)
 {
 	res.send("Hellow world");
 });
+app.get('/me', function(req, res)
+{
+	res.send("@planetofweb");
+});
+
+app.get('/who/:name', function(req, res)
+{
+var name = req.params.name;
+	res.send(name);
+});
+
 
 var server = app.listen(3000, function(){
 	console.log("listening to 3000");
